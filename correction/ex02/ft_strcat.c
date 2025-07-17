3d37839702c2d87234bd8b7001f1106065be0c4c
Kamal Kaced <kkaced@3C7.42angouleme.fr>
2025-07-17 17:40:49 +0200

    caca

---
+++ correction/ex02/ft_strcat.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edi-maio <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 16:41:03 by edi-maio          #+#    #+#             */
+/*   Updated: 2025/07/13 16:57:25 by edi-maio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = ft_strlen(dest);
+	while (src[i] != '\0')
+	{
+		dest[size + i] = src[i];
+		i++;
+	}
+	dest[size + i] = '\0';
+	return (dest);
+}
---
+++ correction/ex05/ft_strlcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edi-maio <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 11:54:17 by edi-maio          #+#    #+#             */
+/*   Updated: 2025/07/15 15:47:20 by edi-maio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	dest_size;
+	unsigned int	src_size;
+
+	i = 0;
+	dest_size = ft_strlen(dest);
+	src_size = ft_strlen(src);
+	if (dest_size >= size)
+		return (size + src_size);
+	while (i < src_size && i < (size - dest_size - 1))
+	{
+		dest[dest_size + i] = src[i];
+		i++;
+	}
+	dest[dest_size + i] = '\0';
+	return (dest_size + src_size);
+}
