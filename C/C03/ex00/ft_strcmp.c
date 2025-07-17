63a66af9b24e78e260e6fc2bf0ecd9903db28bc6
Kamal Kaced <kkaced@3C7.42angouleme.fr>
2025-07-17 15:23:48 +0200

    continue comme ca bg

--- C/C03/ex00/ft_strcmp.c
+++ C/C03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: kkaced <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 14:35:15 by kkaced            #+#    #+#             */
-/*   Updated: 2025/07/17 13:25:09 by kkaced           ###   ########.fr       */
+/*   Updated: 2025/07/17 15:08:07 by kkaced           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,9 @@ int	ft_strcmp(char *str1, char *str2)
 	{
 		i++;
 		if (str2[i] == str1[i])
-		return 0;
-		if (str1[i] = '\0') || (str2[i] = '\0')
-	{
-		return 0;
-	}
+			return 0;
+		if (str1[i] == '\0') 
+			return 0;
 	}
 	tmp = str1[i] - str2[i];
 	return (tmp);
@@ -34,12 +32,12 @@ int	ft_strcmp(char *str1, char *str2)
 #include <string.h>
 int main(void)	
 {
-	printf("\n%d", strcmp("h", "h"));
-/*	printf("\n%d", strcmp("hell11", "hell"));
-	printf("\n%d", ft_strcmp("hell", "hell01"));
-	printf("\n%d", ft_strcmp("hell11", "hell"));
-	printf("\n%d", ft_strcmp("hell11", "hell11"));
-	printf("\n%d", strcmp("hell11", "hell11"));
-*/
+
+	printf("\n%d", strcmp("", "h"));
+	printf("\n%d", ft_strcmp("", "h"));
+	printf("\n%d", ft_strcmp("hell11", ""));
+	printf("\n%d", strcmp("hell11", ""));
+	
+
 	return (0);
 }
--- C/C03/ex01/ft_strncmp.c
+++ C/C03/ex01/ft_strncmp.c
@@ -6,29 +6,33 @@
 /*   By: kkaced <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 18:24:46 by kkaced            #+#    #+#             */
-/*   Updated: 2025/07/16 20:01:12 by kkaced           ###   ########.fr       */
+/*   Updated: 2025/07/17 15:20:48 by kkaced           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *str1, char *str2, unsigned int n)
 {
 	unsigned int	i;
+	unsigned int	tmp;
 
 	i = 0;
-	while (str1[i] == str2[i] && str1[i] != '\0' && i < n)
+	while (str1[i] && str2[i] && i < n)
 	{
 		i++;
+		if (str1[i] == '\0') 
+			return 0;
 	}
-	return ((str1[i] - str2[i]));
+	tmp = str1[i] - str2[i];
+	return (tmp);
 }
-/*
+
 #include <stdio.h>
 #include <string.h>
 int main(void)
 
 {
-	printf("\n%d", strncmp("hell", "hell", 4));
-	printf("\n%d", ft_strncmp("hell0dd", "hell01", 6));
+	printf("\n%d", strncmp("hell0dd", "hell011111", 10));
+	printf("\n%d", ft_strncmp("hell0dd", "hell011111", 10));
 	return (0);
 }
-*/
+
